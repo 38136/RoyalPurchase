9bed503d10e919fcfef16d8cad738cad59135ed0
38136 <38136.hexaware@gmail.com>
2017-08-23 10:18:38 +0530

    Add All files

---
+++ config.js
@@ -0,0 +1,7 @@
+module.exports = {
+    consumer_key: 'r8wS6o9hIl7QUTX2HbGawIIz3',
+    consumer_secret: 'PeRksjl04SBBlxKnvq6OZK6KLnhIyw190NQnTCC2ygERASbjFl',
+    access_token: '898730032381612032-gIEebBsPZ6txCaenCgBf899luwnCaoi',
+    access_token_secret: 'z5B4YUkZI1WET9N071blUjSyQ6fyObaAWvJjOYxKaPD78'
+    // owner_ID : '898730032381612032'
+}
---
+++ index.js
@@ -0,0 +1,47 @@
+console.log("The follow bot is starting");
+
+const Twit = require("twit");
+const express = require("express");
+const bodyParser = require("body-parser");
+let app = express();
+const config = require("./config");
+
+const apiai = require("apiai");
+const APIAII = apiai('8e19b5f4bcee4ca484320e31dfdfebf9');
+let T = new Twit(config);
+const fs = require("fs");
+
+
+let weatherfunc = require('./carfunction');
+let uploadMedia = require("./uploadpic");
+
+let stream = T.stream("user", {
+    stringify_friend_ids: true
+
+});
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+
+// console.log(T);
+
+stream.on('direct_message', function (directMsg) {
+    let directms = directMsg.direct_message;
+    let sender_id = directms.sender_id_str;
+    let screen_name = directms.sender.name;
+    let text = directms.text;
+    let paramssend;
+
+    console.log("Text is " + text);
+    console.log(JSON.stringify(directms.sender));
+    console.log(JSON.stringify(directMsg));
+})
+
+app.get("/", function (req, res) {
+    res.send("success");
+});
+app.listen(process.env.PORT || 3000, function (message) {
+    console.log("Server is running on the port...");
+})
